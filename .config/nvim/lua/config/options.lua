bff7bf266f5c883c1c40bd82ce53aa3c99968af8
Jørgen Høhling Blakey <53434466+HeadCookie@users.noreply.github.com>
2024-12-20 23:51:12 +0100

    remove reference to old dadbod_ui

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -1,8 +1,6 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
--- Configure vim-dadbod-ui
-vim.g.db_ui_save_location = "~/.config/db_ui"
 -- Set to "intelephense" to use intelephense instead of phpactor.
 vim.g.lazyvim_php_lsp = "intelephense"
 require("custom.githunks")
